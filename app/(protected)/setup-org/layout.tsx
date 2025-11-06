c7ae640619b2616dceb69452eb8770efe4949f9e
morsechimwai <morse.chimwai@gmail.com>
2025-11-06 10:00:20 +0700

    feat: remove SetupOrg layout and page components

--- app/(protected)/setup-org/layout.tsx
+++
@@ -1,5 +0,0 @@
-import AuthLayout from "@/app/(auth)/layout"
-
-export default async function SetupOrgLayout({ children }: { children: React.ReactNode }) {
-  return <AuthLayout>{children}</AuthLayout>
-}
