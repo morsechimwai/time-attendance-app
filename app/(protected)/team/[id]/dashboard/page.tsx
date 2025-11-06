a3e671c150be78dc30eb1dc8ebaed4d6711d849b
morsechimwai <morse.chimwai@gmail.com>
2025-11-06 16:36:06 +0700

    feat: update DashboardPage layout for improved styling and consistency

--- app/(protected)/team/[id]/dashboard/page.tsx
+++ app/(protected)/team/[id]/dashboard/page.tsx
@@ -1,6 +1,6 @@
 export default function DashboardPage() {
   return (
-    <div className="p-4">
+    <div className="container mx-auto px-4 lg:px-8 py-8 lg:py-12">
       <h1 className="text-2xl font-bold mb-4">Dashboard Page</h1>
       <p>This is the dashboard page content.</p>
     </div>
--- app/handler/layout.tsx
+++ app/handler/layout.tsx
@@ -1,5 +1,17 @@
+// NextJS
+import { Metadata } from "next"
+
 import { getAuthContext } from "@/lib/auth/context"
 import ProtectedLayout from "../(protected)/layout"
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://face.in"),
+  title: {
+    default: "Account settings",
+    template: "%s | FaceIN",
+  },
+  description: "Dashboard for managing time and attendance efficiently with FaceIN.",
+}
 
 export default async function HandlerLayout({ children }: { children: React.ReactNode }) {
   const ctx = await getAuthContext()
