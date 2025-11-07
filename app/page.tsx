40f3a628994b06ecc95b1eea783e0c31f2aa99d1
morsechimwai <morse.chimwai@gmail.com>
2025-11-07 13:30:30 +0700

    feat: integrate SiteNavbar component into layout and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import SiteNavbar from "@/components/side-navbar"
+
 export default function HomePage() {
   return (
     <div>
+      <SiteNavbar />
       <h1 className="">Home Page</h1>
     </div>
   )
