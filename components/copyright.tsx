956258e6e99bbdaf31bf2ac9f78158da4709ae73
morsechimwai <morse.chimwai@gmail.com>
2025-11-08 09:46:31 +0700

    feat: add Copyright component for displaying copyright information and author link

---
+++ components/copyright.tsx
@@ -0,0 +1,25 @@
+import { CopyrightIcon } from "lucide-react"
+import Link from "next/link"
+
+interface CopyrightProps {
+  className?: string
+}
+
+export default function Copyright({ className }: CopyrightProps) {
+  return (
+    <div
+      className={`flex gap-1 items-center w-full mt-auto text-sm font-sans font-medium ${className}`}
+    >
+      <CopyrightIcon className="size-4" />
+      <span>{new Date().getFullYear()} built with 🩵 by</span>
+      <Link
+        href="https://github.com/morsechimwai"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline underline-offset-4 hover:text-neutral-50"
+      >
+        morsechimwai
+      </Link>
+    </div>
+  )
+}
